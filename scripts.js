a381107902dbf7e30c71ab696abcf6cd484f400b
Mylena Araújo <47645536+myllakassia@users.noreply.github.com>
2023-01-21 18:33:58 -0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,27 @@
+const form = document.querySelector("#form-habits");
+const nlwSetup = new NLWSetup(form);
+const button = document.querySelector("header button");
+
+button.addEventListener("click", add);
+form.addEventListener("change", save);
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5);
+  const dayExists = nlwSetup.dayExists(today);
+
+  if (dayExists) {
+    alert("O dia já existe ⛔");
+    return;
+  }
+
+  alert("Adicionado com sucesso ✅ ");
+  nlwSetup.addDay(today);
+}
+
+function save() {
+  localStorage.setItem("NLWSetupe@habits", JSON.stringify(nlwSetup.data));
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetupe@habits")) || {};
+nlwSetup.setData(data);
+nlwSetup.load();
